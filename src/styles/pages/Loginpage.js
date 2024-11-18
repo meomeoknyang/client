ae33d0fdb51ff72566e528b5ebb28946080551f8
Kyungjin <gulmok1217@hanyang.ac.kr>
2024-11-18 18:01:08 +0900

    feat : 로그인, 회원가입 시 로딩 애니메이션 추가 fix : 회원가입-비밀번호 간단으로 인한 에러 처리

--- src/styles/pages/Loginpage.js
+++ src/styles/pages/Loginpage.js
@@ -180,3 +180,21 @@ export const EmailDomain = styled.span`
   line-height: 0.75rem;
   pointer-events: none;
 `;
+
+export const LoadingOverlay = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.5);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  z-index: 1000;
+`;
+
+export const LoadingImage = styled.img`
+  width: 9.375;
+  height: 9.375;
+`;
